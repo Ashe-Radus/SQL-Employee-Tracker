caff98f8003d0dc0d62382fac5570cfa564e81ae
Scoripo <aradusewicz@gmail.com>
2023-02-21 13:06:51 -0700

    finished server js file

--- BD/seed.sql
+++ BD/seed.sql
@@ -1,8 +1,16 @@
 INSERT INTO list_employee(id, first_name, last_name, role_id, manager_id)
-VALUES ()
+VALUES (1, John, Peach, 23, 10),
+        ( 2, Mary, Blueberry, 27, 10),
+        ( 10, Jerry, Bananas, 30, 11);
 
 INSERT INTO department (id, name_dept)
-VALUES ()
+VALUES (3, Head of Math),
+        (4, Algebra I),
+        (5, Geomerty II);
 
 INSERT INTO employee_role(id, title, salary, department_id)
-VALUES ()
+VALUES (1, teacher, 60000, 4),
+        (2, TA, 30000, 5),  
+        (10, director, 90000, 3);
+
+        
--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+require('dotenv').config();
+
+
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -18,4 +21,162 @@ const db = mysql.createConnection(
     },
     console.log('connected to employee database')
 );
+
+//prompt user for options
+const promptUser = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: 'Choose from the following',
+            choices: [ 'view employees',
+                        'view departments',
+                        'view roles',
+                        'add employee',
+                        'add department',
+                        'add role',
+                        'Done']
+        }
+    ])
+
+    .then((answers) => {
+        const {choices} = answers;
+        if (choices === 'view employees') {
+            showlist_employee();
+        }
+
+        if (choices === 'view departments') {
+            showdepartment();
+        }
+
+        if (choices === 'view roles') {
+            showemployee_role();
+        }
+
+        if (choices === 'add employee') {
+            addemployee();
+        }
+       
+        if (choices === 'add department') {
+            adddepartment();
+        }
+        
+        if (choices === 'add role') {
+            addrole();
+        }
+
+        if (choices === 'done') {
+            Connection.end()
+        };
+
+    });
+};
+
+
+//show employee list 
+
+app.get('/api/list_employee', ({ body}, res) => {
+    const sql = ' SELECT id AS id, last_name AS last_name from list_employee';
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.messagr});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//show department list 
+
+app.get('/api/department', ({ body}, res) => {
+    const sql = ' SELECT id AS id, name_dept AS name_dept from department';
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.messagr});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+
+// show employee roles 
+
+app.get('/api/employee_role', ({ body}, res) => {
+    const sql = ' SELECT id AS id, title AS title from employee_role';
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.messagr});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//add new employee 
+
+app.post('/api/new-employee', ({body}, res) => {
+    const sql = ' INSERT INTO list_employee (first_name, last_name, rold_id, manager_id) VALUES (?)';
+    const params = [body.last_name];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message});
+            return;
+        }
+        res.json({
+            message: 'added employee',
+            data: body
+        });
+    });
+});
+
+//add new department
+
+app.post('/api/new-department', ({body}, res) => {
+    const sql = ' INSERT INTO department (name_dept) VALUES (?)';
+    const params = [body.last_name];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message});
+            return;
+        }
+        res.json({
+            message: 'added department',
+            data: body
+        });
+    });
+});
+
+//add new employee role
+
+app.post('/api/new-employee-role', ({body}, res) => {
+    const sql = ' INSERT INTO employee_role (title, salary, department_id) VALUES (?)';
+    const params = [body.last_name];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message});
+            return;
+        }
+        res.json({
+            message: 'added employee',
+            data: body
+        });
+    });
+});
 
